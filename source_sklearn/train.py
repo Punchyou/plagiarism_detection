3a77d61c5782df2df390ef71d026faf3bd1facf3
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2021-03-22 00:27:38 +0000

    Fix unnessesary arg

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     ## --- Your code here --- ##
 
     ## TODO: Define a model
-    model = KNeighborsClassifier(n_neighbors=n_neighbors)
+    model = KNeighborsClassifier()
 
     ## TODO: Train the model
     model.fit(train_y, train_y)
